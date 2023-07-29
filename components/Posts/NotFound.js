6b57904f147476aa5842e7d6ec00f6ac7bcb62e1
Nacho Huala <manuelhuala@outlook.com>
2023-07-28 20:05:49 -0400

    Not found fixed

--- components/Posts/NotFound.js
+++ components/Posts/NotFound.js
@@ -2,18 +2,23 @@ import Image from "next/image";
 
 const NotFound = () => {
     return (
-        <div className="grid col-span-1 lg:col-span-2 xl:col-span-3 2xl:col-span-4">
+        <div className="grid col-span-2 lg:col-span-2 xl:col-span-3 2xl:col-span-4">
             <div className="flex flex-col mx-auto items-center w-auto lg:flex-row lg:min-h-[60vh] space-y-8">
                 <Image
                     alt="Not Found"
                     src="/webImages/notfound.png"
                     width={0}
                     height={0}
-                    className="w-auto h-64 md:h-5/6"
+                    className="w-auto h-64 md:h-5/6 grayscale"
                 />
-                <h3 className="text-white text-base md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-6xl w-full">
-                    No encontramos nada que coincida :({" "}
-                </h3>
+                <div className="flex flex-col items-center mx-auto">
+                    <h2 className="text-white text-2xl lg:text-3xl xl:text-4xl 2xl:text-6xl font-extrabold">
+                        UPS !
+                    </h2>
+                    <h3 className="text-white text-2xl text-center lg:text-3xl xl:text-4xl 2xl:text-6xl w-full font-gothic italic font-semibold">
+                        No encontramos nada que coincida :({" "}
+                    </h3>
+                </div>
             </div>
         </div>
     );
